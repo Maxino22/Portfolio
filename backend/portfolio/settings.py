ded1c71e3bd1040d0ca801e39090a67a5489b3bb
Maxwell <ajaybullec@gmail.com>
2022-01-02 09:52:04 +0300

    updated settings

--- backend/portfolio/settings.py
+++ backend/portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['blog.maxino.xyz' 'localhost:84', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
